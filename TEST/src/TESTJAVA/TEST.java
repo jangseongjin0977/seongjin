be27f5a5f7b4df4cce9b6ebc153d52af37eb9ea5
SEC <SEC@DESKTOP-AMV59HS>
2021-05-31 21:06:47 +0900

    구구단, 1부터 10까지 더하기

---
+++ TEST/src/TESTJAVA/TEST.java
@@ -0,0 +1,15 @@
+package TESTJAVA;
+
+public class TEST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=2; i<=9; i++) {
+			for(int j=1; j<=9; j++) {
+				System.out.print(i + "*" + j + "=" + i*j + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
